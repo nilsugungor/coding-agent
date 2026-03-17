208acf462b755e11e0ceaf558935e055224c0ce4
nilsugungor <nilsugungor000@gmail.com>
2026-03-17 16:05:06 +0100

    verbose option added

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="coding-agent")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
@@ -27,7 +29,9 @@ response = client.models.generate_content(
 if response.usage_metadata == None:
     raise RuntimeError("Usage metadata none failed api request!!!")
 
-#print(f"User prompt: {response.candidates[0].content}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
