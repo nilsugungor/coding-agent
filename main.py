09de3477189385f3c15615998488f80202530b6a
nilsugungor <nilsugungor000@gmail.com>
2026-03-17 15:56:29 +0100

    added chain of input option with messages

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -16,9 +18,10 @@ parser = argparse.ArgumentParser(description="coding-agent")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
-    model='gemini-2.5-flash-image',
-    contents=args.user_prompt
+    model="gemini-2.5-flash", contents=messages
 )
 
 if response.usage_metadata == None:
@@ -28,4 +31,3 @@ if response.usage_metadata == None:
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
-
