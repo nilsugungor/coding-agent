ad859d9fa602771d05aceedf4ec5a7d8ff3a854f
nilsugungor <nilsugungor000@gmail.com>
2026-03-17 17:33:59 +0100

    adding the system prompt logic

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,7 +23,12 @@ args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=messages
+    model="gemini-2.5-flash",
+    contents=messages,
+    config=types.GenerateContentConfig(
+    system_instruction=system_prompt,
+    temperature=0
+    )
 )
 
 if response.usage_metadata == None:
